0083d2c445d934d6877560a8a508065a1a332c46
TakumiOkada <jak@jakmbp.local>
2022-03-11 07:11:20 +0900

    feat: ondemand isr

---
+++ src/pages/isr60b.tsx
@@ -0,0 +1,51 @@
+import { GetStaticProps, NextPage } from "next";
+import { Box, Flex, Stack, Tag, Text } from "@chakra-ui/react";
+
+import { supabase } from "../utils/supabaseClient";
+import { Hero } from "../components/Hero";
+
+type ContentTable = {
+  id: number;
+  version: number;
+  content: string;
+};
+
+const Isr60B: NextPage<ContentTable> = (props) => {
+  const content = {
+    id: `${props.id}`,
+    version: `${props.version}`,
+    contents: props.content.split("/"),
+  };
+  return (
+    <Flex justify="center" align="center" direction="column">
+      <Hero title={content.version} />
+      <Text fontSize="2xl">id : {content.id}</Text>
+      <Stack spacing={4} mt={6}>
+        {content.contents.map((poke) => (
+          <Box key={poke}>
+            <Tag color="teal.600" py={4} px={8} fontWeight="bold" size="2xl">
+              {poke}
+            </Tag>
+          </Box>
+        ))}
+      </Stack>
+    </Flex>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data, error, status } = await supabase
+    .from<ContentTable>("contents")
+    .select("*")
+    .eq("id", 1)
+    .single();
+
+  if (!data) return { notFound: true };
+
+  return {
+    props: data,
+    revalidate: 60,
+  };
+};
+
+export default Isr60B;
